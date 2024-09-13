6fd36209d76d95aed298872553cda8b798021360
Tane4ka170 <tane4ka170@gmail.com>
2024-09-13 23:34:10 +0300

    Fetched products from database on component mount

---
+++ src/views/CatalogShop.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { onMounted } from 'vue'
+import { useProductsStore } from '@/stores/products'
+
+const productStore = useProductsStore()
+
+onMounted(() => {
+  console.log('first')
+  productStore.fetchProductsFromDB()
+})
+</script>
+
+<template>
+  <h1>Catalog</h1>
+</template>
