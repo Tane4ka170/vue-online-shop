e1ab5668169398fada90b7d4be19f93935c47317
Tane4ka170 <tane4ka170@gmail.com>
2024-09-14 22:37:02 +0300

    Added route for ProductView

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import CatalogShop from '@/views/CatalogShop.vue'
+import ProductView from '@/views/ProductView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: '/',
       name: 'Catalog',
       component: CatalogShop
+    },
+    {
+      path: '/product/:id',
+      name: 'ProductDetail',
+      component: ProductView
     }
   ]
 })
