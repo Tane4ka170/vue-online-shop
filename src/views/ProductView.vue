f8a461cf84bbeecfb50df3008375fd6e40dc551c
Tane4ka170 <tane4ka170@gmail.com>
2024-09-14 22:40:20 +0300

    Added product details view with computed property

---
+++ src/views/ProductView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { computed } from 'vue'
+
+const selectedProduct = computed(getter: () => {
+    return store.products.find((item)=>item.id===id)
+})
+</script>
+
+<template>
+  <h1>Product Details</h1>
+</template>
