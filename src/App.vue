a7e32ecc8a0488b9cb27555f6f2cf5e0542fb889
Tane4ka170 <tane4ka170@gmail.com>
2024-09-15 23:32:17 +0300

    Refactored cart click handler

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,18 @@ import { useProductsStore } from './stores/products'
 
 const router = useRouter()
 const store = useProductsStore()
+
+const handleCartClick = () => {
+  if (router) {
+    router.push({ name: 'CartView' })
+  }
+}
 </script>
 
 <template>
   <header>
-    <div class="cart-items" @click="router.push({ name: 'CartView' })">
-      <p>Items in cart: {{ store.cart?.length || 0 }}</p>
+    <div class="cart-items" @click="handleCartClick">
+      <p>Items in cart: {{ store.cart?.length }}</p>
     </div>
   </header>
   <main><RouterView /></main>
