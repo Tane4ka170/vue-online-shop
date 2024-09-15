03c6f1e30a169744d018b9407e09f0dbb5d411c3
Tane4ka170 <tane4ka170@gmail.com>
2024-09-15 22:16:34 +0300

    Added product detail page

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -1,11 +1,37 @@
 <script setup>
 import { computed } from 'vue'
+import { useProductsStore } from '@/stores/products'
+import { useRoute } from 'vue-router'
 
-const selectedProduct = computed(getter: () => {
-    return store.products.find((item)=>item.id===id)
+const store = useProductsStore()
+const route = useRoute()
+
+const selectedProduct = computed(() => {
+  return store.products.find((item) => item.id === Number(route.params.id))
 })
 </script>
 
 <template>
-  <h1>Product Details</h1>
+  <div class="product" v-if="selectedProduct">
+    <div class="product-image">
+      <img :src="selectedProduct.thumbnail" alt="Product Image" />
+    </div>
+    <div class="product-details">
+      <p>Brand: {{ selectedProduct.brand }}</p>
+      <p>Description: {{ selectedProduct.description }}</p>
+      <h2>Price: ${{ selectedProduct.price }}</h2>
+    </div>
+  </div>
+  <p v-else>Product not found.</p>
 </template>
+
+<style scoped>
+.product {
+  display: flex;
+  margin-top: 50px;
+}
+
+.product-image {
+  margin-right: 24px;
+}
+</style>
