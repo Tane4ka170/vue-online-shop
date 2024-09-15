f542cc1770045003a2ac97a3bad4c44c6b970862
Tane4ka170 <tane4ka170@gmail.com>
2024-09-15 22:51:00 +0300

    Added header with cart items count and navigation

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,26 @@
-<script setup></script>
+<script setup>
+import { useRouter } from 'vue-router'
+import { useProductsStore } from './stores/products'
+
+const router = useRouter()
+const store = useProductsStore()
+</script>
 
 <template>
+  <header>
+    <div class="cart-items" @click="router.push({ name: 'CartView' })">
+      <p>Items in cart: {{ store.cart?.length || 0 }}</p>
+    </div>
+  </header>
   <main><RouterView /></main>
 </template>
 
-<style scoped></style>
+<style scoped>
+.cart-items {
+  text-align: end;
+  padding: 16px;
+  font-weight: bold;
+  font-size: 24px;
+  cursor: pointer;
+}
+</style>
